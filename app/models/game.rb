fea0e5b23425b3f149c0bcb2a8270f05f590dfcf
Brent Busby <brent.busby@yahoo.com>
2018-09-11 06:52:59 -0500

    fix month off by 1 error in game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,13 +10,11 @@ class Game < ApplicationRecord
                    "September", "October", "November", "December"]
     CSV.foreach(file.path, headers: true) do |line|
       date = line[0]
-      if line[6] == 'CHN'
-        Game.create(game_year: date[(0..3)], park: line[6], home_team: 'Chicago National',
+        @game = Game.create(game_year: date[(0..3)], park: line[6], home_team: line[6],
                     away_team: "#{line[3]} #{line[4]}", home_homeruns: line[53],
                     away_homeruns: line[25], total_homeruns: (line[53] + line[3]),
-                    day_of_week: line[2], month: month_names[date[(4..5)].to_i],
-                    season: Season.where(year: date[(0..3)]).first, team: Team.where(sheet_key: line[6]), team: Team.where(sheet_key: line[3]))
-      end
+                    day_of_week: line[2], month: month_names[(date[(4..5)].to_i - 1)],
+                    season: Season.where(year: date[(0..3)]).first)
       # do a case statement to translate each team name, park
     end
   end
