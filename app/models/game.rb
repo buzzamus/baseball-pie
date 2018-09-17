65ed1045a2945ba20912040446e8f81e1ceefbdb
Brent Busby <brent.busby@yahoo.com>
2018-09-17 06:54:15 -0500

    automate creating teams in upload through game association

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,8 +10,8 @@ class Game < ApplicationRecord
                    "September", "October", "November", "December"]
     CSV.foreach(file.path, headers: true) do |line|
       date = line[0]
-        @game = Game.create(game_year: date[(0..3)], park: line[6], home_team: line[6],
-                    away_team: line[3], home_homeruns: line[53],
+        @game = Game.create(game_year: date[(0..3)], park: line[6], home_team: Team.where(sheet_key: line[6]).first_or_create(league: line[7], sheet_key: line[6]),
+                    away_team: Team.where(sheet_key: line[3]).first_or_create(league: line[4], sheet_key: line[3]), home_homeruns: line[53],
                     away_homeruns: line[25], total_homeruns: (line[53] + line[3]),
                     day_of_week: line[2], month: month_names[(date[(4..5)].to_i - 1)],
                     season: Season.where(year: date[(0..3)]).first)
