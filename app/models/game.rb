cbb78badbf3b2884cd7de562a26a8292e47296b1
Brent Busby <brent.busby@yahoo.com>
2018-09-19 07:07:35 -0500

    automate adding team city in create

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,8 +13,8 @@ class Game < ApplicationRecord
         @game = Game.create(
           game_year: date[(0..3)],
           park: line[6],
-          home_team: Team.where(sheet_key: line[6]).first_or_create(league: line[7], sheet_key: line[6]).sheet_key,
-          away_team: Team.where(sheet_key: line[3]).first_or_create(league: line[4], sheet_key: line[3]).sheet_key,
+          home_team: Team.where(sheet_key: line[6]).first_or_create(league: line[7], sheet_key: line[6], city: self.city_adder(line[6].to_sym)).sheet_key,
+          away_team: Team.where(sheet_key: line[3]).first_or_create(league: line[4], sheet_key: line[3], city: self.city_adder(line[3].to_sym)).sheet_key,
           home_homeruns: line[53],
           away_homeruns: line[25],
           total_homeruns: (line[53] + line[3]),
@@ -23,5 +23,43 @@ class Game < ApplicationRecord
           season: Season.where(year: date[(0..3)]).first
         )
     end
+  end
+
+  private
+  # need to add previous team names (such as Florida Marlins) for older seasons
+  def self.city_adder(arg)
+    cities = {
+      TBA: "Tampa Bay",
+      TOR: "Toronto",
+      PIT: "Pittsburgh",
+      SLN: "St. Louis",
+      ANA: "Los Angeles",
+      CHN: "Chicago",
+      BAL: "Baltimore",
+      MIN: "Minnesota",
+      OAK: "Oakland",
+      CHA: "Chicago",
+      TEX: "Texas",
+      SEA: "Seattle",
+      ARI: "Arizona",
+      COL: "Colorado",
+      ATL: "Atlanta",
+      WAS: "Washington",
+      CIN: "Cincinnati",
+      PHI: "Philadelphia",
+      MIL: "Milwaukee",
+      SFN: "San Francisco",
+      SDN: "San Diego",
+      LAN: "Los Angeles",
+      CLE: "Cleveland",
+      BOS: "Boston",
+      KCA: "Kansas City",
+      NYN: "New York",
+      NYA: "New York",
+      HOU: "Houston",
+      MIA: "Miami",
+      DET: "Detroit"
+    }
+    return cities[arg]
   end
 end
